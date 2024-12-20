022d3e971e9820130aa447c1468464b94b35112d
motz0815 <me@motz.xyz>
2024-12-20 21:02:53 +0100

    format everything with prettier

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,20 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
 
-const prod = process.env.NODE_ENV === "production";
+const prod = process.env.NODE_ENV === "production"
 
-const internalHost = process.env.TAURI_DEV_HOST || "localhost";
+const internalHost = process.env.TAURI_DEV_HOST || "localhost"
 
 const nextConfig: NextConfig = {
-  // Ensure Next.js produces a static export instead of doing SSR
-  // https://nextjs.org/docs/app/building-your-application/deploying/static-exports
-  output: "export",
-  // Note: This feature is required to use the Next.js Image component in SSG mode
-  // See https://nextjs.org/docs/messages/export-image-api for different workarounds
-  images: {
-    unoptimized: true,
-  },
-  // Configure assetPrefix or else the server won't properly resolve your assets in dev mode
-  assetPrefix: prod ? undefined : `http://${internalHost}:3000`,
-};
+    // Ensure Next.js produces a static export instead of doing SSR
+    // https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+    output: "export",
+    // Note: This feature is required to use the Next.js Image component in SSG mode
+    // See https://nextjs.org/docs/messages/export-image-api for different workarounds
+    images: {
+        unoptimized: true,
+    },
+    // Configure assetPrefix or else the server won't properly resolve your assets in dev mode
+    assetPrefix: prod ? undefined : `http://${internalHost}:3000`,
+}
 
-export default nextConfig;
+export default nextConfig
